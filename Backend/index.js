bf99ac42d5154af501e9454681270f65ab8cc7c8
ColinCrasta <56228104+ColinCrasta@users.noreply.github.com>
2023-03-04 18:41:35 -0500

    Backend set up

---
+++ Backend/index.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+const cors = require('cors');
+const express = require('express');
+const app = express();
+const outputURL = require('./functions/OutputURL');
+
+
+// Uses port number that is given in an
+// env file or port 5000 otherwise
+const PORT = process.env.PORT || 5000;
+
+
+// Sets the view engine to ejs
+app.set('view engine', 'ejs');
+
+
+// Outputs the type of  request,
+// endpoint, and time of the request
+app.use(outputURL);
+
+
+// Middleware that will be used
+app.use(cors()); //Allows requests from various origins to be handled 
+app.use(express.json({ limit: '100mb' })) //creates a req.body that can be handled easily. The 10 mb limit is to not include large requests
+app.use(express.urlencoded({ extended: false })); //Allows us to parse data from forms
+
+
+// Routes
+app.get('/', (req, res) => {
+  //res.send('Hello World!')
+  const name = 'John';
+  res.render('index.ejs', { name });
+})
+
+
+
+// Listens to a specific port number 
+// and displays the port number
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`)
+});
