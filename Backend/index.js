1dcea5ece78a1b2e237ce8d7fc55a86c673bcbba
ColinCrasta <56228104+ColinCrasta@users.noreply.github.com>
2023-03-04 18:53:28 -0500

    dotenv is fixed

--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,5 @@
-require('dotenv').config();
+const path = require('path');
+require('dotenv').config({ path: path.join(path.join(__dirname, '..'), '.env') });
 const cors = require('cors');
 const express = require('express');
 const app = express();
@@ -7,7 +8,7 @@ const outputURL = require('./functions/OutputURL');
 
 // Uses port number that is given in an
 // env file or port 5000 otherwise
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORTBACKEND || 5000;
 
 
 // Sets the view engine to ejs
